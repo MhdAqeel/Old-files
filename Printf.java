59a9622667b74380bf638ff1d89357c5ee553d3b
Rahuman Noor Mohamed Aqeel <mohamedaqeel1130@gmail.com>
2025-09-15 22:27:42 +0530

    Add files via upload

---
+++ Printf.java
@@ -0,0 +1,7 @@
+public class Printf{
+	public static void main (String [] args){
+		String name = "Aqeel";
+		int age = 2;
+		System.out.printf("Hello, %s. Next year, you'll be %d", name, age);
+	}
+}
